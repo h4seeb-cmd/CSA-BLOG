05c925f9cb12c96cbc60800d8252c9c50bb28145
h4seeb-cmd <mirzahbeg123@gmail.com>
2024-01-08 00:03:52 -0800

    adding env proof imgs

---
+++ test.js
@@ -0,0 +1,7 @@
+require('dotenv').config({ path: '/home/m1rza/vscode/CSA-BLOG/2024-testFile.env' });
+
+// Access environment variables
+ const apiKey = process.env.API_KEY;
+
+// Display the secrets
+ console.log("API Key:", apiKey);
